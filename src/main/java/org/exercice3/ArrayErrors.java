3bcdb5e07fbb11cbef26b197383a58b89ff71e57
geridev <publiger30@gmail.com>
2025-03-13 13:57:55 +0100

    rev1

--- src/main/java/org/exercice3/ArrayErrors.java
+++ src/main/java/org/exercice3/ArrayErrors.java
@@ -4,7 +4,7 @@ public class ArrayErrors {
 
     public static ArrayIndexOutOfBoundsException getIncorrectArrayIndex(int[] array) {
         try {
-            int incorrectIndex = array[array.length + 1];
+            int incorrectIndex = array[array.length];
             return null;
         } catch (ArrayIndexOutOfBoundsException e) {
             return e;
